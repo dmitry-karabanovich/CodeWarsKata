acd27f77d0a43b46020ca31988631e65789601e8
Dmitry Karabanovich <dmitry.karabanovich@hiqo-solutions.com>
2020-02-05 15:19:53 +0300

    Resolve 7 kyu "Friend or Foe?" kata

---
+++ CodeWarsKata/Kata.Test/7 kyu kata/Kata_7_1_Test - Copy.cs
@@ -0,0 +1,16 @@
+﻿using Kata._7_kyu_kata;
+using Xunit;
+
+namespace Kata.Test._7_kyu_kata
+{
+    public class Kata_7_2_Test
+    {
+        [Fact]
+        public void RemoveOnlyNamesWithFourCharacters()
+        {
+            string[] expected = { "Ryan", "Mark" };
+            string[] names = { "Ryan", "Kieran", "Mark", "Jimmy" };
+            Assert.Equal(expected, Kata_7_2.FriendOrFoe(names));
+        }
+    }
+}
---
+++ CodeWarsKata/Kata/7 kyu kata/Kata_7_2.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata._7_kyu_kata
+{
+    public class Kata_7_2
+    {
+        public static IEnumerable<string> FriendOrFoe(string[] names)
+        {
+            return names.Where(x => x.Length == 4);
+        }
+    }
+}
