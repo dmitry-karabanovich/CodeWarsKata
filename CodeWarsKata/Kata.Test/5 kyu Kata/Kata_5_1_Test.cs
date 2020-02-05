ce5f5053aa30fde0ab99c50ba82473634d77acdb
Dmitry Karabanovich <dmitry.karabanovich@hiqo-solutions.com>
2020-02-05 18:03:45 +0300

    resolved 5 kyu "Maximum subarray sum" kata

---
+++ CodeWarsKata/Kata.Test/5 kyu Kata/Kata_5_1_Test.cs
@@ -0,0 +1,26 @@
+﻿using Kata._5_kyu_Kata;
+using Xunit;
+
+namespace Kata.Test._5_kyu_Kata
+{
+    public class Kata_5_1_Test
+    {
+        [Fact]
+        public void MaxSequenceIsZero()
+        {
+            Assert.Equal(0, Kata_5_1.MaxSequence(new int[0]));
+        }
+
+        [Fact]
+        public void TeMaxSequenceIsSix()
+        {
+            Assert.Equal(6, Kata_5_1.MaxSequence(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
+        }
+
+        [Fact]
+        public void TeMaxSequenceIsSeven()
+        {
+            Assert.Equal(7, Kata_5_1.MaxSequence(new int[] { 2, -3, 3, 4, -5, 2}));
+        }
+    }
+}
---
+++ CodeWarsKata/Kata/5 kyu Kata/Kata_5_1.cs
@@ -0,0 +1,22 @@
+﻿namespace Kata._5_kyu_Kata
+{
+    public class Kata_5_1
+    {
+        //https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c
+        public static int MaxSequence(int[] arr)
+        {
+            var sum = 0;
+            var maxSum = 0;
+            foreach (var t in arr)
+            {
+                sum += t;
+                if (sum > maxSum)
+                    maxSum = sum;
+                if (sum < 0)
+                    sum = 0;
+            }
+
+            return maxSum;
+        }
+    }
+}
